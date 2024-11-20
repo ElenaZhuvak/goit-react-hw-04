3dd6d2a216a0205df94a53794e4e4fd71bb830f1
Elena Zhuvak <elenkazh@gmail.com>
2024-11-20 22:41:28 +0000

    M4 Lesson1 practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,32 @@
 import 'modern-normalize'
-
+import { useEffect, useState } from 'react'
+import Articles from './Articles/Articles';
+import { fetchArticles } from '../services/api';
+import Loader from './Loader/Loader';
 
 const App = () => {
+    const [articles, setArticles] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                setIsLoading(true)
+                const {hits}= await fetchArticles()
+                setIsLoading(false)
+                setArticles(hits)
+            } catch (error) {
+                console.error(error)
+            }
+        }        
+        getData()
+    }, []);
+
   return (
-    <div>App</div>
+    <div>
+        {isLoading && <Loader />}
+        <Articles articles={articles}/>
+    </div>
   )
 }
 
