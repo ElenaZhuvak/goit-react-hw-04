8ea778827158291435fd528379380df0b0b35331
Elena Zhuvak <elenkazh@gmail.com>
2024-11-25 22:39:44 +0000

    adds modal

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,8 +1,28 @@
-
+import Modal from 'react-modal'
 
 const ImageModal = ({image, onOpen, onClose}) => {
+    const customStyles = {
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+        },
+      };
   return (
-    <Modal></Modal>
+    <Modal
+        onOpen={onOpen}
+        onAfterOpen={image}
+        onClose={onClose}
+        style={customStyles}
+    >
+    {image && (
+        <img src={image.urls.regular}
+        alt={image.alt_description} />
+    )}
+    </Modal>
   )
 }
 
