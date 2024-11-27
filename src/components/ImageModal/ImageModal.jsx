f9e28be1fe689972c4ab4d931ccb2112a7a20eeb
Elena Zhuvak <elenkazh@gmail.com>
2024-11-27 17:46:58 +0000

    adds modal close option

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,29 +1,13 @@
 import Modal from 'react-modal'
 import css from './ImageModal.module.css'
 
-const ImageModal = ({image, isOpen, onClose}) => {
-    const customStyles = {
-        content: {
-          top: '50%',
-          left: '50%',
-          right: 'auto',
-          bottom: 'auto',
-          marginRight: '-50%',
-          transform: 'translate(-50%, -50%)',
-          padding: '0',
-          border: 'none'
-        },
-        overlay: {
-            backgroundColor: 'rgba(0, 0, 0, 0.8)'
-        }
-      };
-
+const ImageModal = ({image, isOpen, onClose, style}) => {
+   
   return (
     <Modal
         isOpen={isOpen}
-        onAfterOpen={image}
-        onClose={onClose}
-        style={customStyles}
+        onRequestClose={onClose}
+        style={style}
     >
     {image && (
         <img className={css.modalImg} src={image.urls.regular}
